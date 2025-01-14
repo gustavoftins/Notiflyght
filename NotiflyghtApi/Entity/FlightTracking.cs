6f26d09ffd664af5826489af6793ffba253b9b9b
gustavoftins <gustavofrmartins44@gmail.com>
2025-01-13 21:31:58 -0300

    Initial scheme

---
+++ NotiflyghtApi/Entity/FlightTracking.cs
@@ -0,0 +1,8 @@
+namespace NotiflyghtApi.Entity;
+
+public class FlightTracking
+{
+    public string NotificationEmail { get; set; }
+    public string FlightNumber { get; set; }
+    public DateTime FlightDate { get; set; }
+}
