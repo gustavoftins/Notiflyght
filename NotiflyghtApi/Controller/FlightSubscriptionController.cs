b04d348657a8b71d497b92d5ef28b61d4790fba2
gustavoftins <gustavofrmartins44@gmail.com>
2025-01-14 20:30:35 -0300

    Controller scheme

--- NotiflyghtApi/Controller/FlightSubscriptionController.cs
+++ NotiflyghtApi/Controller/FlightSubscriptionController.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using NotiflyghtApi.Entity;
+
 namespace NotiflyghtApi.Controller;
 
-public class FlightSubscriptionController
+[ApiController]
+[Route("api/[controller]")]
+public class FlightSubscriptionController : ControllerBase
 {
-    
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] FlightSubscription flightSubscription)
+    {
+        return Ok();
+    }
+
+    [HttpPatch]
+    public async Task<IActionResult> Patch([FromBody] FlightSubscription flightSubscription)
+    {
+        return Ok();
+    }
 }
